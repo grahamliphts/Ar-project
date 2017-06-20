c9806c4d7cdda3f3e126775a172ade86184b8054
Kevin Rey <k.rey@hotmail.fr>
2017-06-20 21:45:15 +0200

    Scripts Units and Gamemanager

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    // --------------------------------------------------
+    // Liste unités - 0 = Tank - 1 = Heal - 2 et 3 = DPS
+    // --------------------------------------------------
+    [SerializeField]
+    UnitsScripts[] _unitsPlayer1;
+
+    [SerializeField]
+    UnitsScripts[] _unitsPlayer2;
+    // --------------------------------------------------
+
+    void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Synchronisation();
+        }
+    }
+
+    public UnitsScripts FindTargetCloser(Transform positionSource, int playerToSearch)
+    {
+        float distance = -1;
+        UnitsScripts target = null;
+
+        UnitsScripts[] unitsToSearch = (playerToSearch == 1) ? _unitsPlayer1 : _unitsPlayer2;
+        foreach (UnitsScripts unit in unitsToSearch)
+        {
+            if (unit.IsAlive())
+            {
+                Transform positionTarget = unit.GetPosition();
+
+                float newDistance = Mathf.Sqrt(Mathf.Pow(positionTarget.position.x - positionSource.position.x, 2) + Mathf.Pow(positionTarget.position.z - positionSource.position.z, 2));
+                if (distance == -1 || newDistance < distance)
+                {
+                    distance = newDistance;
+                    target = unit;
+                }
+            }
+        }
+
+        return target;
+    }
+
+    public UnitsScripts FindAllyToHeal(Transform positionSource, int playerToSearch)
+    {
+        float distance = -1;
+        UnitsScripts target = null;
+
+        UnitsScripts[] unitsToSearch = (playerToSearch == 1) ? _unitsPlayer1 : _unitsPlayer2;
+        foreach (UnitsScripts unit in unitsToSearch)
+        {
+            if (unit.IsAlive() && unit.IsDamaged())
+            {
+                Transform positionTarget = unit.GetPosition();
+
+                float newDistance = Mathf.Sqrt(Mathf.Pow(positionTarget.position.x - positionSource.position.x, 2) + Mathf.Pow(positionTarget.position.y - positionSource.position.y, 2));
+                if (distance == -1 || newDistance < distance)
+                {
+                    distance = newDistance;
+                    target = unit;
+                }
+            }
+        }
+
+        return target;
+    }
+
+    public void Synchronisation()
+    {
+        for(int i = 3; i >= 0; i--)
+        {
+            for (int player = 1; player <= 2; player++)
+            {
+                UnitsScripts unit = (player == 1 ? _unitsPlayer1[i] : _unitsPlayer2[i]);
+
+                unit.Alignement();
+            }
+        }
+    }
+
+}
---
+++ Assets/Script/UnitsScripts.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitsScripts : MonoBehaviour
+{
+    public enum Role
+    {
+        Tank,
+        Heal,
+        Dps
+    }
+    public Role _role;
+
+    [SerializeField]
+    GameManager _gameManager;
+
+    [SerializeField]
+    int _team;
+
+    [SerializeField]
+    int _pv;
+
+    [SerializeField]
+    int _damage;
+
+    private int pvInitial;
+
+    void Start()
+    {
+        pvInitial = _pv;
+    }
+
+    public void Alignement()
+    {
+        bool healDone = false;
+        if(_role == Role.Heal)
+        {
+            UnitsScripts targetHeal = _gameManager.FindAllyToHeal(transform, _team);
+
+            if (targetHeal != null)
+            {
+                targetHeal.ApplyHeal(_damage);
+                healDone = true;
+                Debug.Log("Heal Team" + _team + " ship : " + targetHeal.name);
+            }
+        }
+
+        if((_role != Role.Heal) || (_role == Role.Heal && !healDone))
+        {
+            UnitsScripts target = _gameManager.FindTargetCloser(transform, _team == 1 ? 2 : 1);
+
+            if(target != null)
+            {
+                // Alignement Model
+                // TODO
+
+                // Fire + Particles
+                Fire(target);
+            }
+        }
+    }
+   
+    private void Fire(UnitsScripts target)
+    {
+        Debug.Log(name + " attack " + target.name);
+        target.ApplyDamage(_damage);
+    }
+
+
+
+    public bool IsAlive()
+    {
+        return _pv > 0 ? true : false;
+    }
+
+    public bool IsDamaged()
+    {
+        return _pv != pvInitial ? true : false;
+    }
+
+    public Transform GetPosition()
+    {
+        return transform;
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        _pv -= damage;
+    }
+
+    public void ApplyHeal(int heal)
+    {
+        if (_pv + heal > pvInitial)
+            _pv = pvInitial;
+        else
+            _pv += heal;
+    }
+
+}
